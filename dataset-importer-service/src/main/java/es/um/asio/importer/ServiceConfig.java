de84ee13f824c6c428f82efab118350cff8ad8a6
IZERTIS\alvaro.blanco <alvaro.blanco@izertis.com>
2021-06-21 08:48:18 +0200

    [SEMANTMURC-657] Adaptación del dataset importer para servir endpoints rest

---
+++ dataset-importer-service/src/main/java/es/um/asio/importer/ServiceConfig.java
@@ -0,0 +1,13 @@
+package es.um.asio.importer;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Service Spring configuration.
+ */
+@Configuration
+@ComponentScan
+public class ServiceConfig {
+
+}
